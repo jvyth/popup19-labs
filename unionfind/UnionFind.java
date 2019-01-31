c9844ec99a8177bee49477afd28c15cdbb6cfef0
Jakob Vyth <vyth@kth.se>
2019-01-31 11:50:55 +0100

    use buffered out

--- unionfind/UnionFind.java
+++ unionfind/UnionFind.java
@@ -1,57 +1,80 @@
-import java.util.ArrayList;
+/*
+  This file contains a solution for https://kth.kattis.com/problems/unionfind
+  Created during spring semester of 2019 for the course DD2458 at KTH
+  Authors: Jakob Vyth (vyth@kth.se) and Carl Nyströmer (carlnys@kth.se)
+*/
+
+import java.util.Arrays;
 
 public class UnionFind {
-  private class Node {
-    int val;
-    public Node parent;
+  private int[] parents;
+  private int[] size;
 
-    public Node(int x) {
-      this.val = x;
-      this.parent = this;
-    }
-
-    public boolean isRoot() {
-      return this == this.parent;
+  public UnionFind(int n) {
+    parents = new int[n];
+    size = new int[n];
+    for (int i = 0; i < n ; ++i) {
+      parents[i] = i;
+      size[i] = 1;
     }
   }
 
-  private ArrayList<Node> sets;
-
-  public UnionFind(int initSize) {
-    sets = new ArrayList<>(initSize);
+  public boolean same(int x, int y) {
+    return find(x) == find(y);
   }
 
-  Node find(int x) {
-    try {
-      Node n = sets.get(x);
-      if (n == null) return null;
-      while(!n.isRoot()) {
-        Node tmp = n;
-        n = n.parent;
-        tmp.parent = n.parent;
+  private int find(int x) {
+    if (parents[x] != x) parents[x] = find(parents[x]);
+    return parents[x];
+  }
+
+  // private int find(int x) {
+  //   while (parents[x] != x) x = parents[x];
+  //   return x;
+  // }
+
+  // private int find(int x) {
+  //
+  //   while (parents[x] != x) {
+  //     int tmp = x;
+  //     x = parents[x];
+  //     parents[tmp] = parents[x];
+  //   }
+  //   return x;
+  // }
+
+  int union(int x, int y) {
+    int xRoot = find(x);
+    int yRoot = find(y);
+    if (xRoot == yRoot) return xRoot;
+    if (size[xRoot] > size[yRoot]) {
+      parents[yRoot] = xRoot;
+      size[xRoot] += size[yRoot];
+      return xRoot;
+    } else {
+      parents[xRoot] = yRoot;
+      size[yRoot] += size[xRoot];
+      return yRoot;
+    }
+  }
+
+  public String toString() {
+    return Arrays.toString(parents);
+  }
+
+  public static void main(String[] args) {
+    Kattio io = new Kattio(System.in, System.out);
+    int N = io.getInt(); int Q = io.getInt();
+    UnionFind uf = new UnionFind(N);
+    String cmd; int a,b;
+    for (;Q > 0; --Q) {
+      cmd = io.getWord(); a = io.getInt(); b = io.getInt();
+      if (cmd.equals("?")) {
+        io.println(uf.same(a,b) ? "yes" : "no");
+      } else {
+        uf.union(a,b);
       }
-      return n;
-    } catch (IndexOutOfBoundsException e) {
-      return null;
     }
-  }
-
-  boolean makeSet(int x) {
-    try {
-      if (sets.get(x) != null) return false;
-      sets.set(x, new Node(x));
-      return true;
-    } catch (IndexOutOfBoundsException e) {
-      sets.ensureCapacity(x+1);
-      return makeSet(x);
-    }
-  }
-
-  Node union(int x, int y) {
-    Node xr = find(x);
-    Node yr = find(y);
-    if (xr == null || yr == null) return null;
-    yr.parent = xr;
-    return xr;
+    io.close();
   }
 }
